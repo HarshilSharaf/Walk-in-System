e1ef1b71f7b6e1c96a2904b3e5f61d9906e52490
Harshil Sharaf <harshil.sharaf1234@gmail.com>
2022-02-18 11:58:03 +0530

    Inital UI setup

---
+++ frontend/src/App.js
@@ -0,0 +1,56 @@
+import { createContext, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Grid, makeStyles } from "@material-ui/core";
+
+import Navbar from "./component/Navbar";
+import Login from "./component/Login";
+import Signup from "./component/Signup";
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "98vh",
+    paddingTop: "64px",
+    boxSizing: "border-box",
+    width: "100%",
+  },
+}));
+
+export const SetPopupContext = createContext();
+
+function App() {
+  const classes = useStyles();
+  const [popup, setPopup] = useState({
+    open: false,
+    severity: "",
+    message: "",
+  });
+  return (
+    <BrowserRouter>
+      <SetPopupContext.Provider value={setPopup}>
+        <Grid container direction="column">
+          <Grid item xs>
+            <Navbar />
+          </Grid>
+          <Grid item className={classes.body}>
+            <Switch>
+              <Route exact path="/">
+                <Login />
+              </Route>
+
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+              </Switch>
+          </Grid>
+        </Grid>
+      
+      </SetPopupContext.Provider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
